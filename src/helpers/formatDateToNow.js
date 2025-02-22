3fa33351c68d5dc539f687764d979c21bfbfe815
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2025-02-22 10:21:14 -0500

    update1

---
+++ src/helpers/formatDateToNow.js
@@ -0,0 +1,8 @@
+import { formatDistanceToNow } from 'date-fns'
+
+
+export const formatDateToNow = (postedAt) => {
+  return (
+   formatDistanceToNow(new Date( postedAt ), { addSuffix: true, })
+  )
+}
